bb4ab9b0743106c1fb078bb55d3610a499a18dc7
Attila Fulop <fulopattila122@gmail.com>
2017-11-29 18:36:04 +0200

    Added missing concord manifest

---
+++ tests/ConcordModuleTest.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Contains the ConcordModuleTest class.
+ *
+ * @copyright   Copyright (c) 2017 Attila Fulop
+ * @author      Attila Fulop
+ * @license     MIT
+ * @since       2017-11-29
+ *
+ */
+
+
+namespace Vanilo\Address\Tests;
+
+
+class ConcordModuleTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function dependent_concord_modules_are_present()
+    {
+        $modules = $this->app->concord
+            ->getModules()
+            ->keyBy(function($module) {
+                return $module->getId();
+            });
+
+        $this->assertTrue($modules->has('konekt.address'), 'Address module should be registered');
+
+        $this->assertTrue(
+            $modules->get('vanilo.address')
+                    ->getManifest()
+                    ->getKind()
+                    ->isModule(),
+            'Concord Module Type Should be a module'
+        );
+    }
+
+}
